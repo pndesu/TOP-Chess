65b3396fa59c8d8a75f2eae83c52e5b982e3f3ec
pndesu <tranvanphucnguyen1@gmail.com>
2023-01-08 18:52:16 +1100

    Include basic move function

--- main.rb
+++ main.rb
@@ -60,6 +60,37 @@ module Action
             King.new('black', position)
         end
     end
+
+    def move(position)
+
+    end
+
+    def get_square(position) #Don't need to be in white/black class? If moved to and called in Square class in white/black turn, check square.side, if match caller side, proceed?
+        x_axis = ['8', '7', '6', '5', '4', '3', '2', '1']
+        y_axis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        position = position.split('')
+        position = position.map do |value|
+            if y_axis.include?(value)
+                value = y_axis.find_index(value)
+            elsif x_axis.include?(value)
+                value = x_axis.find_index(value)
+            end
+        end
+        Board.board[position[1]][position[0]]
+    end
+
+    def move_to_new_square(old_position, new_position)
+        old_square = get_square(old_position)
+        new_square = get_square(new_position)
+        new_square.piece = old_square.piece
+        new_square.side = old_square.side
+        reset_square(old_square)
+    end
+
+    def reset_square(square)
+        square.piece = ' '
+        square.side = ''
+    end
 end
 
 class White
@@ -92,10 +123,10 @@ class Pawn
         @side = side
         if side == 'white'
             @symbol = '♙'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♙')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♙', 'white')}
         else
             @symbol = '♟︎'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♟︎')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♟︎', 'black')}
         end
     end
 end
@@ -105,10 +136,10 @@ class Knight
         @side = side
         if side == 'white'
             @symbol = '♘'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♘')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♘', 'white')}
         else
             @symbol = '♞'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♞')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♞', 'black')}
         end
     end
 end
@@ -118,10 +149,10 @@ class Bishop
         @side = side
         if side == 'white'
             @symbol = '♗'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♗')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♗', 'white')}
         else
             @symbol = '♝'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♝')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♝', 'black')}
         end
     end
 end
@@ -131,10 +162,10 @@ class Rook
         @side = side
         if side == 'white'
             @symbol = '♖'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♖')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♖', 'white')}
         else
             @symbol = '♜'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♜')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♜', 'black')}
         end
     end
 end
@@ -144,10 +175,10 @@ class Queen
         @side = side
         if side == 'white'
             @symbol = '♕'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♕')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♕', 'white')}
         else
             @symbol = '♛'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♛')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♛', 'black')}
         end
     end
 end
@@ -157,19 +188,20 @@ class King
         @side = side
         if side == 'white'
             @symbol = '♔'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♔')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♔', 'white')}
         else
             @symbol = '♚'
-            position.each{|square| Board.board[square[0]][square[1]].update_piece('♚')}
+            position.each{|square| Board.board[square[0]][square[1]].update_piece('♚', 'black')}
         end
     end
 end
 
 class Square
-    attr_accessor :color, :piece
-    def initialize(color, piece = ' ', position = [])
+    attr_accessor :color, :piece, :side
+    def initialize(color, piece = ' ', position = [], side = '') #Is position necessary?
         @color = color
         @piece = piece
+        @side = side
     end
 
     def display_square
@@ -177,17 +209,20 @@ class Square
         return square_white if color == 'white'
     end
 
-    def update_piece(piece)
+    def update_piece(piece, side)
         @piece = piece
+        @side = side
         display_square
     end
 
     def square_dark
-        "\u001b[48;5;95m #{@piece} \u001b[0m"
+        # "\u001b[48;5;95m #{@piece} \u001b[0m"
+        "\e[46m #{@piece} \e[0m"
     end
     
     def square_white
-        "\u001b[48;5;223m #{@piece} \u001b[0m"
+        # "\u001b[48;5;223m #{@piece} \u001b[0m"
+        "\e[47m #{@piece} \e[0m"
     end
 end
 
@@ -201,7 +236,7 @@ class Board
         for i in 0..7
             row = []
             for j in 0..7
-                row << Square.new('white', ' ', [i,j]) if i.odd? && j.odd?
+                row << Square.new('white', ' ', [i,j]) if i.odd? && j.odd? #Position only used here
                 row << Square.new('dark', ' ', [i,j]) if i.odd? && j.even?
                 row << Square.new('white', ' ', [i,j]) if i.even? && j.even?
                 row << Square.new('dark', ' ', [i,j]) if i.even? && j.odd?
@@ -225,10 +260,10 @@ class Game
     def play
         board = Board.new
         board.reset_board
-        White.new
-        Black.new
+        white = White.new
+        black = Black.new
+        white.move_to_new_square('a2', 'a4')
         board.display_board
     end
 end
-
 Game.new.play
--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,10 +1,26 @@
-square = "\u001b[46m ♙ \u001b[0m"
-square1 = "\u001b[46m ♟︎ \u001b[0m"
-opposite_square1 = "\u001b[47m ♟︎ \u001b[0m"
-square2 = "\u001b[48;5;95m  \u001b[0m"
-opposite_square2 = "\u001b[48;5;223m ♙ \u001b[0m"
-print(square1)  
-print(opposite_square1)
-print "\n"
-print(opposite_square2)
-print(square2)
+# square = "\u001b[46m ♙ \u001b[0m"
+# square1 = "\u001b[46m ♟︎ \u001b[0m"
+# opposite_square1 = "\u001b[47m ♟︎ \u001b[0m"
+# square2 = "\u001b[48;5;95m  \u001b[0m"
+# opposite_square2 = "\u001b[48;5;223m ♙ \u001b[0m"
+# print(square1)  
+# print(opposite_square1)
+# print "\n"
+# print(opposite_square2)
+# print(square2)
+
+def map_axis(position)
+    x_axis = ['1', '2', '3', '4', '5', '6', '7', '8']
+    y_axis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    position = position.split('')
+    position = position.map do |value|
+        if y_axis.include?(value)
+            value = y_axis.find_index(value)
+        elsif x_axis.include?(value)
+            value = x_axis.find_index(value)
+        end
+    end
+    p position
+end
+
+map_axis('a4')
